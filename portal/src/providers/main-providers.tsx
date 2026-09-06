4f60d3e26a9a18614b71876344bf504016b1776e
Shaid Azmin <excel.azmin@gmail.com>
2026-09-06 12:26:42 +0000

    remove hardcoded url

--- portal/src/providers/main-providers.tsx
+++ portal/src/providers/main-providers.tsx
@@ -9,7 +9,7 @@ const MainProviders = ({ children }: { children: React.ReactNode }) => {
   return (
     <FrappeProvider
       enableSocket={false}
-      url='http://localhost:8001'
+      
       {...(VITE_FRAPPE_URL && VITE_FRAPPE_TOKEN
         ? {
             url: VITE_FRAPPE_URL,
